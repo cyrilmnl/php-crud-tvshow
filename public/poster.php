dd1ce197ab4153e7efff4a4c17b5248a153e9804
mani0022 <cyril.manil@etudiant.univ-reims.fr>
2022-05-31 15:13:17 +0200

    Création de poster.php et de l'exception ParameterException

---
+++ public/poster.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Entity\Poster;
+use Entity\Exception\EntityNotFoundException;
+use Entity\Exception\ParameterException;
+
+try {
+    if (isset($_GET["id"]) && ctype_digit($_GET["id"]) == false) {
+        throw new ParameterException("No data found");
+    }
+    $posterId = Poster::findById((int)$_GET["id"]);
+    header("Content-type: image/jpeg");
+    echo $posterId->getJpeg();
+} catch (ParameterException) {
+    http_response_code(400);
+} catch (EntityNotFoundException) {
+    http_response_code(404);
+} catch (Exception) {
+    http_response_code(500);
+}
