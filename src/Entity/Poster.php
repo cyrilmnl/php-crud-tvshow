aa3ed62a9362d963607fc82f0c06e330c7c5d5ee
mani0022 <cyril.manil@etudiant.univ-reims.fr>
2022-05-31 11:30:23 +0200

    Création de la classe Poster

---
+++ src/Entity/Poster.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace Entity;
+
+use Database\MyPdo;
+use PDO;
+
+class Poster
+{
+    private int $id;
+    private string $jpeg;
+
+    /** Assesseur de l'id de la classe Poster
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /** Assesseur du Jpeg de la classe Poster
+     *
+     * @return string
+     */
+    public function getJpeg(): string
+    {
+        return $this->jpeg;
+    }
+
+    public static function findById(int $id): Poster
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT *
+            FROM poster
+            WHERE id = :id
+        SQL
+        );
+
+        $stmt->execute([":id" => $id]);
+        $stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, Poster::class);
+
+        $fetch = $stmt->fetch();
+        if ($fetch == false) {
+            throw new EntityNotFoundException("No data found");
+        }
+        return $fetch;
+    }
+
+
+
+}
