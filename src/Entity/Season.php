76f49d5d293ac7902c91e27eede09ed113d2f04c
mani0022 <cyril.manil@etudiant.univ-reims.fr>
2022-05-31 17:31:48 +0200

    Ajout de la méthode findById dans la classe Season

--- src/Entity/Season.php
+++ src/Entity/Season.php
@@ -3,6 +3,10 @@ declare(strict_types=1);
 
 namespace Entity;
 
+use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
+use PDO;
+
 class Season
 {
     private int $id;
@@ -60,5 +64,30 @@ class Season
     public function getPosterId(): ?int
     {
         return $this->posterId;
+    }
+
+    /** Méthode permettant d'accéder à une saison par son id
+     *
+     * @param int $id
+     * @return Season
+     */
+    public static function findById(int $id): Season
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT *
+            FROM season
+            WHERE id = :id
+        SQL
+        );
+
+        $stmt->execute([":id" => $id]);
+        $stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, Season::class);
+
+        $fetch = $stmt->fetch();
+        if ($fetch == false) {
+            throw new EntityNotFoundException("No data found");
+        }
+        return $fetch;
     }
 }
