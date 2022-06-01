cd08142ffc6076429a6eaf21c86b8a8dcd0e2b3c
mani0022 <cyril.manil@etudiant.univ-reims.fr>
2022-06-01 13:35:10 +0200

    Création du fichier tvshow-form.php

---
+++ public/admin/tvshow-form.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Entity\Exception\EntityNotFoundException;
+use Entity\Exception\ParameterException;
+use Entity\Tvshow;
+
+try {
+    if (isset($_GET["tvShowId"])) {
+        if (ctype_digit($_GET["tvShowId"]) == false) {
+            throw new ParameterException("No data found");
+        } else {
+            $tvShowId = Tvshow::findById((int)$_GET["tvShowId"]);
+        }
+    } else {
+        $tvShowId = null;
+    }
+} catch (ParameterException) {
+    http_response_code(400);
+} catch (EntityNotFoundException) {
+    http_response_code(404);
+} catch (Exception) {
+    http_response_code(500);
+}
