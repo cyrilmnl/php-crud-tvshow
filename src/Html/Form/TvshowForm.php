074a679715add5ed4b287dbe0b19d4c5caba2dfe
mani0022 <cyril.manil@etudiant.univ-reims.fr>
2022-06-01 09:10:51 +0200

    Ajout de la méthode getHtmlForm dans la classe TvshowForm

--- src/Html/Form/TvshowForm.php
+++ src/Html/Form/TvshowForm.php
@@ -17,5 +17,37 @@ class TvshowForm
         $this->tvshow = $tvshow;
     }
 
-    
+    /** Assesseur d'une série dans la classe TvshowForm
+     *
+     * @return Tvshow|null
+     */
+    public function getTvshow(): ?Tvshow
+    {
+        return $this->tvshow;
+    }
+
+    /** Méthode produisant le code HTML du formulaire
+     *
+     * @param string $action
+     * @return string
+     */
+    public function getHtmlForm(string $action): string
+    {
+        $action = self::escapeString($action);
+        $nom = self::escapeString($this->getTvshow()?->getName());
+        $content = <<<HTML
+                    <form method="post" action="{$action}">
+                        <label>
+                            Nom
+                            <input type="text" name="name" value="{$nom}" required>
+                        </label>
+                        
+                        <input type="hidden" name="id" value="{$this->getArtist()?->getId()}">
+                        
+                        <button type="submit">Enregistrer</button>
+                    </form>
+        HTML;
+
+        return $content;
+    }
 }
