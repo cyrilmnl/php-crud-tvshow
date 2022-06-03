ad90f642a5e8aa509721223b0377f9efd319e49a
olse0002 <matheo.olsen@etudiant.univ-reims.fr>
2022-06-03 09:18:35 +0200

    TvshowSave -> Reformat - Ajout de commentaire permettant un meilleure lisibilité du code - Suppression de la redondance d'initialisation de la classe WebPage

--- public/admin/tvshow-save.php
+++ public/admin/tvshow-save.php
@@ -7,28 +7,36 @@ use Entity\Tvshow;
 use Html\Form\TvshowForm;
 use Html\WebPage;
 
-$pageweb = new WebPage("Formulaire");
+/*
+ * Initialisation de l'objet WebPage et définition du titre de la page
+ */
+$pageweb = new WebPage();
+
+/*
+* Définition du titre de la page
+*/
+$pageweb->setTitle("Formulaire de création ou d'édition d'une série");
+
+/*
+ * Ajout de la feuille de style
+ */
+$pageweb->appendCssUrl("../css/styles.css");
 
 if (!isset($_GET["tvShowId"])) {
 
-
     /*
-     * Définition du titre de la page
+     * Initilisation d'un formulaire avec tvshow null en
+     * paramètre car tvShowId n'est pas défini
      */
-    $pageweb->setTitle("Formulaire de création ou d'édition d'une série");
-
-    $pageweb->appendCssUrl("../css/styles.css");
-
-    /*
-     * FORMULAIRE VIDE CAR PAS SET
-     */
-
     $form = new TvshowForm(null);
+
+    /*
+     * Ajout d'un formulaire vide dans le corps du Html
+     */
     $pageweb->appendContent($form->getHtmlForm("tvshow-form.php", true));
 
     echo $pageweb->toHTML();
 } else {
-
     if (ctype_digit($_GET["tvShowId"]) == false) {
         throw new ParameterException("No data found");
     } else {
@@ -36,16 +44,21 @@ if (!isset($_GET["tvShowId"])) {
     }
 
     /*
-     * Définition du titre de la page
+     * Initilisation d'un Tvshow grâce à l'ID récupéré
+     * grâce à la méthode GET
      */
-    $pageweb->setTitle("Formulaire de création ou d'édition d'une série");
-
-    $pageweb->appendCssUrl("../css/styles.css");
-
-
     $tvshow = Tvshow::findById(($tvShowId));
 
+    /*
+     * Initilisation d'un formulaire avec le Tvshow
+     * précédemment initialisé
+     */
     $form = new TvshowForm($tvshow);
+
+    /*
+     * Ajout d'un formulaire remplit avec les données
+     * du Tvshow précédemment initialisé
+     */
     $pageweb->appendContent($form->getHtmlForm("tvshow-form.php?tvShowId={$tvShowId}", false));
 
     echo $pageweb->toHTML();
